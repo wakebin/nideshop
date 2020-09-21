da96cabc0a369ea589dc003de54b49f559d60229
wakebin <bin-wei@foxmail.com>
2020-09-21 20:44:50 +0800

    little change

--- development.js
+++ development.js
@@ -1,15 +1,15 @@
 const Application = require('thinkjs');
-const babel = require('think-babel');
+// const babel = require('think-babel');
 const watcher = require('think-watcher');
-const notifier = require('node-notifier');
+// const notifier = require('node-notifier');
 
 const instance = new Application({
   ROOT_PATH: __dirname,
   watcher: watcher,
-  transpiler: [babel, {
-    presets: ['think-node']
-  }],
-  notifier: notifier.notify.bind(notifier),
+  // transpiler: [babel, {
+  //   presets: ['think-node']
+  // }],
+  // notifier: notifier.notify.bind(notifier),
   env: 'development'
 });
 
